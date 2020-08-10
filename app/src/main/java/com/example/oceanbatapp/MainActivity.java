b56ff75cdede76f01cf388b74eb5956f856b429a
moklee88 <cuihaomok88@gmail.com>
2020-08-10 09:00:01 +0800

    navigation in progress

--- app/src/main/java/com/example/oceanbatapp/MainActivity.java
+++ app/src/main/java/com/example/oceanbatapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.oceanbatapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer);
     }
 }
