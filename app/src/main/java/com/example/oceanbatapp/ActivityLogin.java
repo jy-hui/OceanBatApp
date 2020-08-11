1f3a495a81e9505e6a5aa5af276fd71d2be6b1e6
moklee88 <cuihaomok88@gmail.com>
2020-08-11 22:34:02 +0800

    Fixing ActivityLogin.java

--- app/src/main/java/com/example/oceanbatapp/ActivityLogin.java
+++ app/src/main/java/com/example/oceanbatapp/ActivityLogin.java
@@ -1,34 +1,16 @@
 package com.example.oceanbatapp;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
-
-import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputLayout;
-
-import android.text.style.TtsSpan;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity {
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputPassword;
-
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    Toolbar toolbar;
-    NavigationView navigationView;
 
 
     @SuppressLint("WrongViewCast")
@@ -37,11 +19,10 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-
-
         textInputEmail = findViewById(R.id.text_input_email);
         textInputPassword = findViewById(R.id.text_input_password);
     }
+
     private boolean validateEmail(){
         String emailInput;
         emailInput =(textInputEmail.getEditText()).getText().toString().trim();
@@ -66,6 +47,8 @@ public class login extends AppCompatActivity {
             return true;
         }
     }
+
+
     public void confirmInput(View v) {
         if(!validateEmail()|!validatePassword()){
             return;
@@ -76,4 +59,7 @@ public class login extends AppCompatActivity {
         Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 
+    public void LoginInput(View view) {
+
+    }
 }
--- app/src/main/java/com/example/oceanbatapp/MainActivity.java
+++ app/src/main/java/com/example/oceanbatapp/MainActivity.java
@@ -1,40 +1,21 @@
 package com.example.oceanbatapp;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.wifi.hotspot2.pps.HomeSp;
+
 import android.os.Bundle;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.textfield.TextInputLayout;
-
-import android.text.style.TtsSpan;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
-    private TextInputLayout textInputEmail;
-    private TextInputLayout textInputPassword;
-
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    Toolbar toolbar;
-    NavigationView navigationView;
-
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+
     }
+
 }
