b68a7de99497e606e3c3ae1db140c31b613d9326
USER <laubclcs-wm19@student.tarc.edu.my>
2020-08-18 14:52:34 +0800

    Done layout

--- app/src/main/java/com/example/oceanbatapp/ActivityLogin.java
+++ app/src/main/java/com/example/oceanbatapp/ActivityLogin.java
@@ -2,13 +2,14 @@ package com.example.oceanbatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.textfield.TextInputLayout;
 import android.view.View;
-
+import android.widget.Button;
 import android.widget.Toast;
 
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity implements View.OnClickListener {
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputPassword;
 
@@ -19,9 +20,13 @@ public class ActivityLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        //textInputEmail = findViewById(R.id.text_input_email);
+        final Button Login = findViewById(R.id.Login_button);
+        Login.setOnClickListener(this);
+
+       // textInputEmail = findViewById(R.id.text_input_email);
         //textInputPassword = findViewById(R.id.text_input_password);
     }
+
 
     private boolean validateEmail(){
         String emailInput;
@@ -34,6 +39,7 @@ public class ActivityLogin extends AppCompatActivity {
             textInputEmail.setError(null);
             return true;
         }
+
     }
 
     private boolean validatePassword(){
@@ -49,6 +55,7 @@ public class ActivityLogin extends AppCompatActivity {
     }
 
 
+
     public void confirmInput(View v) {
         if(!validateEmail()|!validatePassword()){
             return;
@@ -61,5 +68,18 @@ public class ActivityLogin extends AppCompatActivity {
 
     public void LoginInput(View view) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.Login_button: {
+                Toast.makeText(this, "Button Login clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ActivityLogin.this, Home.class);
+                startActivity(intent);
+                break;
+            }
+        }
     }
 }
