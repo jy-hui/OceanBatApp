5919dc4ced252ed8344c774041a7d97359b2fac2
moklee88 <cuihaomok88@gmail.com>
2020-09-01 17:34:57 +0800

    navigation drawer almost done

--- app/src/main/java/com/example/oceanbatapp/NavigationDrawer.java
+++ app/src/main/java/com/example/oceanbatapp/NavigationDrawer.java
@@ -1,20 +1,38 @@
 package com.example.oceanbatapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.annotation.SuppressLint;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class NavigationDrawer extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.drawer_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout= findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_text,R.string.close_text);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
     }
 
