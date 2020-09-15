e44b9f0f2c0c90c7567b5f44a68389eec9324fd4
jy-hui <huijy-wm19@student.tarc.edu.my>
2020-09-15 12:50:41 +0800

    Commit by Hui Jun Yoong

--- app/src/main/java/com/example/oceanbatapp/History.java
+++ app/src/main/java/com/example/oceanbatapp/History.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class History extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
